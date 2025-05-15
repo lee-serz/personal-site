eee6697a0df7798e6fd8e1bf6ed02ee3f6ada42c
Lee Sergey <serzh.li.04.06@yandex.ru>
2025-05-16 01:57:19 +0300

    init

---
+++ src/app/components/welcome/Welcome.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import Image from "next/image";
+import s from "./Welcome.module.scss";
+import Typed from "typed.js";
+import React, { useEffect, useState } from "react";
+
+export default function Welcome() {
+  const el = React.useRef(null);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.matchMedia("(max-width: 900px)").matches);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["веб-приложения"], // Одна строка
+      typeSpeed: 100, // Скорость печати
+      backSpeed: 30, // Скорость стирания
+      loop: true, // Бесконечный цикл
+      showCursor: true, // Показывать курсор
+      cursorChar: "|", // Символ курсора
+      backDelay: 1500, // Задержка перед стиранием
+    });
+  }, []);
+
+  return (
+    <main className={s.welcome}>
+      <div className={s.greeting}>
+        {/* 1 */}
+        <p
+          className={`${s.greeting__text} animate__animated animate__fadeInLeft animate__delay-1s`}
+        >
+          Привет!
+        </p>
+        {/* 2 */}
+        <div
+          className={`${s.greeting__image} animate__animated  animate__zoomIn animate__delay-2s`}
+        >
+          <Image
+            src="/hand.png"
+            fill
+            alt="Эмодзи Рука"
+            quality={100}
+            className={`animate__animated animate__tada  animate__delay-2s`}
+          />
+        </div>
+      </div>
+      <div className={s.info}>
+        <p
+          className={`${s.info__text} animate__animated animate__jackInTheBox animate__delay-3s`}
+        >
+          Меня зовут <span className={s.info__bold}>Сергей</span>.
+          {isMobile && <br />} Я превращаю идеи
+          <br />в современные {isMobile && <br />}
+          <span ref={el} className={s.info__highlight}></span>
+        </p>
+        <div
+          className={`${s.info__telegram}  animate__animated animate__zoomIn animate__delay-5s `}
+        >
+          <a
+            className={`${s.info__link}`}
+            href="https://t.me/derante"
+            target="__blank"
+          >
+            <Image
+              src={"/telegram-black.png"}
+              fill
+              alt="Телеграм"
+              quality={100}
+              className="animate__animated animate__flip animate__delay-5s"
+            />
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Sora } from "next/font/google";
+import "./reset.css";
+import "animate.css";
+import "hover.css/css/hover-min.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const sora = Sora({
+  variable: "--font-sora",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="ru">
+      <body className={sora.variable}>{children}</body>
     </html>
   );
 }
